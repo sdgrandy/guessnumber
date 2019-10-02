41d9f7e3c7b9f4600becc889d50893f15f2987ac
Seth Grandy <10246031@my.uvu.edu>
2019-10-02 15:42:08 -0500

    push to remote

---
+++ guessnumber.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	var guess, number int
+	rand.Seed(time.Now().UnixNano())
+	number = 1 + rand.Intn(10)
+	fmt.Print("Guess a number between 1 and 10: ")
+	fmt.Scanf("%d", &guess)
+	if number == guess {
+		fmt.Println("You guessed correctly.")
+	} else {
+		fmt.Println("You guessed incorrectly.")
+		fmt.Printf("The number was %d.\n", number)
+	}
+}
